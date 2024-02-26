466956deb86c7a0565774d04b70c324469aaa585
CBIS2023Manuel <157013710+CBIS2023Manuel@users.noreply.github.com>
2024-02-26 10:45:06 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,25 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 10; index++) {
-        music.setTempo(120)
-        music.play(music.tonePlayable(330, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-        if (input.lightLevel() > 90) {
-            music.play(music.tonePlayable(440, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-        } else if (input.lightLevel() >= 90) {
-            music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-        }
-        music.rest(music.beat(BeatFraction.Whole))
-        music.play(music.stringPlayable("C5 A B E A G D C ", 120), music.PlaybackMode.UntilDone)
+    music.setTempo(120)
+    music.play(music.tonePlayable(330, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    if (input.lightLevel() > 90) {
+        music.play(music.tonePlayable(440, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    } else if (input.lightLevel() >= 90) {
+        music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     }
+    music.play(music.stringPlayable("C5 A B E A G D C ", 120), music.PlaybackMode.UntilDone)
+    basic.showIcon(IconNames.EighthNote)
 })
-input.onButtonPressed(Button.AB, function () {
-    basic.showLeds(`
-        . . # . .
-        . . # # .
-        . . # . #
-        # # # . .
-        # # # . .
-        `)
+input.onSound(DetectedSound.Loud, function () {
+    music.play(music.stringPlayable("C5 B E F D C D E ", 120), music.PlaybackMode.UntilDone)
 })
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 4; index++) {
-    	
+    music.setTempo(120)
+    music.play(music.tonePlayable(165, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    if (input.lightLevel() > 100) {
+        music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    } else if (input.lightLevel() >= 100) {
+        music.play(music.tonePlayable(196, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     }
+    music.play(music.stringPlayable("C5 E B C E G D B ", 120), music.PlaybackMode.UntilDone)
+    basic.showIcon(IconNames.QuarterNote)
 })
